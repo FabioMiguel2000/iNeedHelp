d811c034511282a356422d428c24e0db45bec6e7
Pedro Pacheco <up201806824@fe.up.pt>
2021-11-18 15:50:41 +0000

    Auto stash before merge of "main" and "origin/main"

--- create.sql
+++ create.sql
@@ -1,60 +1,87 @@
-DROP TABLE IF EXISTS users CASCADE;
-DROP TABLE IF EXISTS questions CASCADE;
-DROP TABLE IF EXISTS answers CASCADE;
-DROP TABLE IF EXISTS comments CASCADE;
-DROP TABLE IF EXISTS tags CASCADE;
-DROP TABLE IF EXISTS badges CASCADE;
-DROP TABLE IF EXISTS user_badges CASCADE;
+DROP TABLE IF EXISTS User;
+DROP TABLE IF EXISTS AuthenticatedUser;
+DROP TABLE IF EXISTS Visitor;
+DROP TABLE IF EXISTS Author;
+DROP TABLE IF EXISTS Moderator;
+DROP TABLE IF EXISTS Administrator;
+DROP TABLE IF EXISTS QuestionAuthor;
+DROP TABLE IF EXISTS Question;
+DROP TABLE IF EXISTS Tag;
+DROP TABLE IF EXISTS Image;
+DROP TABLE IF EXISTS Badge;
+DROP TABLE IF EXISTS Awser;
+DROP TABLE IF EXISTS Upvoted;
+DROP TABLE IF EXISTS Comment;
 
-DROP TYPE IF EXISTS user_role;
-
-CREATE TYPE user_role AS ENUM ('Author', 'Moderator', 'Administrator')
-CREATE TYPE badge_tier AS ENUM ('Bronze', 'Silver', 'Gold')
-CREATE TYPE user_status AS ENUM ('Offline', 'Online', 'Idle', 'DoNotDisturb')
-
-CREATE TABLE users (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    nickname varchar(15),
-    password varchar(20),
-    email varchar(40),
-    registerDate date,
-    isBlocked boolean,
-    status user_status, --status type enum
-    bio varchar(300),
-    location varchar(50)
+CREATE TYPE BadgeType AS ENUM (
+    'goldBadge',
+    'silverBadge',
+    'bronzeBadge'
+);
+CREATE TYPE StatusType AS ENUM (
+    'active',
+    'inactive',
+    'idle',
+    'doNotDisturb'
+)
+CREATE TABLE User();
+CREATE TABLE AuthenticatedUser (
+    userId VARCHAR(64) NOT NULL UNIQUE,
+    nickname VARCHAR(25) NOT NULL UNIQUE,
+    userpassword TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE CHECK(VALUE LIKE '%@%._%'),
+    registerDate DATE NOT NULL DEFAULT CURRENT_TIMESTAMP() CHECK(registerDate >= CURRENT_TIME),
+    isBlocked INTEGER DEFAULT 0,
+    statusType StatusType NOT NULL,
+    bio TEXT,
+    userlocation TEXT,
+    CONSTRAINT blockedBoolean CHECK (isBlocked == 0 OR isBlocked == 1) 
 );
 
 CREATE TABLE Visitor();
-CREATE TABLE Author();
-CREATE TABLE Moderator();
-CREATE TABLE Administrator();
-CREATE TABLE QuestionAuthor();
-CREATE TABLE Question(
-    questionId varchar(80),
-    createdDate date,
-    title varchar(30),
-    content varchar(300),
-    views int
+CREATE TABLE Author(
+    auth_user PRIMARY KEY REFERENCES AuthenticatedUser ON UPDATE CASCADE ON DELETE SET NULL
 );
-CREATE TABLE tags(
-    tagname varchar(15)
+CREATE TABLE Moderator(
+    auth_user PRIMARY KEY REFERENCES AuthenticatedUser ON UPDATE CASCADE ON DELETE SET NULL
+);
+CREATE TABLE Administrator(
+    auth_user PRIMARY KEY REFERENCES AuthenticatedUser ON UPDATE CASCADE ON DELETE SET NULL
+);
+CREATE TABLE QuestionAuthor(
+    author PRIMARY KEY REFERENCES Author ON UPDATE CASCADE ON DELETE SET NULL
+);
+CREATE TABLE Question(
+    questionId VARCHAR(64) NOT NULL UNIQUE,
+    createdDate DATE NOT NULL DEFAULT CURRENT_TIMESTAMP() CHECK(createdDate >= CURRENT_TIME),
+    title VARCHAR(35) NOT NULL,
+    content TEXT NOT NULL,
+    views INTEGER NOT NULL CHECK(views >= 0)
+);
+CREATE TABLE Tag(
+    tagname VARCHAR(25) NOT NULL
 );
 CREATE TABLE Image(
-    path varchar(80)
+    imagePath TEXT NOT NULL
 );
 CREATE TABLE badges(
-    type badgetype, --badge type enum
-    receivedDate date,
-    title varchar(15),
-    content varchar(25)
+    badgeType BadgeType NOT NULL,
+    receivedDate DATE NOT NULL DEFAULT CURRENT_TIMESTAMP() CHECK(receivedDate >= CURRENT_TIME),
+    title VARCHAR(25) NOT NULL,
+    content VARCHAR(35) NOT NULL
 );
 
-CREATE TABLE answers (
-    lasEditedDate date,
-    content varchar(500)
+CREATE TABLE Awser (
+    lasEditedDate DATE NOT NULL DEFAULT CURRENT_TIMESTAMP() CHECK(lasEditedDate >= CURRENT_TIME),
+    content VARCHAR(500) NOT NULL
 );
 
 CREATE TABLE Upvoted (
-    likes int,
-    dislikes int
+    likes INTEGER NOT NULL CHECK(likes >= 0),
+    dislikes INTEGER NOT NULL CHECK(dislikes >= 0)
+);
+
+CREATE TABLE Comment (
+    lasEditedDate DATE NOT NULL DEFAULT CURRENT_TIMESTAMP() CHECK(lasEditedDate >= CURRENT_TIME),
+    content TEXT NOT NULL
 );
