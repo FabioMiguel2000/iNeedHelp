cd339981e2202a087eee54a891fafddcef02fc16
Ivo Ribeiro <up201307718@alunos.dcc.fc.up.pt>
2021-11-22 15:00:34 +0000

    started populating with users and some changes on the create script

--- populate.sql
+++ populate.sql
@@ -1,4 +1,9 @@
 /*User*/
+INSERT INTO "users"(nickname, full_name, email, password, role, isblocked, status, bio, location, created_at ,updated_at) VALUES
+('jhonnyB', 'Jhonny Bravo', 'up197400007@up.pt', 'supersecretpassword', 'Author', DEFAULT, DEFAULT, 'suck at math...', 'Porto', DEFAULT, DEFAULT),
+('hunnidGrams', 'Filipe Gomes', 'up143300000@up.pt', 'banana123', 'Author', DEFAULT, DEFAULT, 'ready to code! :)', 'Algarve', DEFAULT, DEFAULT),
+('sanchovies', 'Karim Badjoras', 'up196900001@up.pt', '0000', 'Moderator', DEFAULT, 'idle', 'here to check typos only', 'Curral de Moinas', DEFAULT, DEFAULT),
+('lugaRythm', 'Rui Pinto', 'up000000042@up.pt', 'admin', 'Administrator', DEFAULT, 'doNotDisturb', '42 is the solution to all questions', 'Oiã', DEFAULT, DEFAULT);
 
 /*AuthenticatedUser*/
 
