05ebc6a1b2ee78949f60d9afea1afd97b20836f6
Ivo Ribeiro <up201307718@alunos.dcc.fc.up.pt>
2021-11-22 15:27:49 +0000

    rolled back some changes (SERIAL -> UUID)

--- create.sql
+++ create.sql
@@ -3,23 +3,24 @@ DROP SCHEMA IF EXISTS lbaw2153 			CASCADE;
 CREATE SCHEMA lbaw2153;
 SET search_path TO lbaw2153;
 
-DROP TYPE IF EXISTS "badge_type" 		CASCADE;
-DROP TYPE IF EXISTS "status_type" 		CASCADE;
-DROP TYPE IF EXISTS "user_role" 		CASCADE;
 
-DROP TABLE IF EXISTS "users" 			CASCADE;
-DROP TABLE IF EXISTS "questions" 		CASCADE;
-DROP TABLE IF EXISTS "tags" 			CASCADE;
-DROP TABLE IF EXISTS "question_tags" 	CASCADE;
-DROP TABLE IF EXISTS "answers" 			CASCADE;
-DROP TABLE IF EXISTS "comments" 		CASCADE;
+DROP TYPE IF EXISTS "badge_type" CASCADE;
+DROP TYPE IF EXISTS "status_type" CASCADE;
+DROP TYPE IF EXISTS "user_role" CASCADE;
 
-DROP TABLE IF EXISTS "Image" 			CASCADE;
+DROP TABLE IF EXISTS "users" CASCADE;
+DROP TABLE IF EXISTS "questions" CASCADE;
+DROP TABLE IF EXISTS "tags" CASCADE;
+DROP TABLE IF EXISTS "question_tags" CASCADE;
+DROP TABLE IF EXISTS "answers" CASCADE;
+DROP TABLE IF EXISTS "comments" CASCADE;
 
-DROP TABLE IF EXISTS "badges" 			CASCADE;
-DROP TABLE IF EXISTS "user_badges" 		CASCADE;
+DROP TABLE IF EXISTS "Image" CASCADE;
 
-DROP TABLE IF EXISTS "upvotable" 		CASCADE;
+DROP TABLE IF EXISTS "badges" CASCADE;
+DROP TABLE IF EXISTS "user_badges" CASCADE;
+
+DROP TABLE IF EXISTS "upvotable" CASCADE;
 
 CREATE TYPE "badge_type" AS ENUM (
     'goldBadge',
@@ -42,7 +43,7 @@ CREATE TYPE "user_role" AS ENUM (
 
 CREATE TABLE "users"
 (
-    id         SERIAL PRIMARY KEY  ,
+    id         UUID PRIMARY KEY      DEFAULT gen_random_uuid(),
 
     nickname   VARCHAR(25)  NOT NULL UNIQUE CHECK ( length(nickname) >= 3 ),
     full_name  VARCHAR(100),
@@ -91,7 +92,7 @@ CREATE TABLE "upvotable"
 
 CREATE TABLE "questions"
 (
-    id         SERIAL PRIMARY KEY ,
+    id         UUID PRIMARY KEY        DEFAULT gen_random_uuid(),
 
     title      VARCHAR(100)   NOT NULL CHECK ( length(title) >= 10 ),
     content    VARCHAR(10000) NOT NULL CHECK ( length(content) >= 20 ),
@@ -100,27 +101,27 @@ CREATE TABLE "questions"
     created_at TIMESTAMP      NOT NULL DEFAULT now(),
     updated_at TIMESTAMP      NOT NULL DEFAULT now(),
     CONSTRAINT ck_updated_after_created CHECK ( updated_at >= created_at )
-) INHERITS (upvotable);
+) INHERITS ("upvotable");
 
 CREATE TABLE "tags"
 (
-    id   SERIAL PRIMARY KEY,
+    id   UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(30) NOT NULL CHECK ( length(name) >= 1 )
 );
 
 CREATE TABLE "question_tags"
 (
     PRIMARY KEY (question_id, tag_id),
-    question_id SERIAL REFERENCES "questions"(id) ON UPDATE CASCADE,
-    tag_id      SERIAL REFERENCES "tags" (id) ON UPDATE CASCADE
+    question_id UUID REFERENCES "questions"(id) ON UPDATE CASCADE,
+    tag_id      UUID REFERENCES "tags"(id) ON UPDATE CASCADE
 );
 
 CREATE TABLE "answers"
 (
-    id          SERIAL PRIMARY KEY  ,
+    id          UUID PRIMARY KEY        DEFAULT gen_random_uuid(),
 
-    user_id     SERIAL           NOT NULL REFERENCES users (id) ON UPDATE CASCADE,
-    question_id SERIAL          NOT NULL REFERENCES questions (id) ON UPDATE CASCADE,
+    user_id     UUID           NOT NULL REFERENCES "users"(id) ON UPDATE CASCADE,
+    question_id UUID           NOT NULL REFERENCES "questions"(id) ON UPDATE CASCADE,
     CONSTRAINT ck_one_answer_per_user UNIQUE (user_id, question_id),
 
     content     VARCHAR(10000) NOT NULL CHECK ( length(content) >= 20 ),
@@ -132,11 +133,11 @@ CREATE TABLE "answers"
 
 CREATE TABLE "comments"
 (
-    id          SERIAL PRIMARY KEY ,
-    user_id     SERIAL          NOT NULL REFERENCES users (id) ON UPDATE CASCADE,
+    id          UUID PRIMARY KEY       DEFAULT gen_random_uuid(),
+    user_id     UUID          NOT NULL REFERENCES "users"(id) ON UPDATE CASCADE,
 
-    question_id SERIAL REFERENCES questions (id) ON UPDATE CASCADE,
-    answer_id   SERIAL REFERENCES answers (id) ON UPDATE CASCADE,
+    question_id UUID REFERENCES "questions"(id) ON UPDATE CASCADE,
+    answer_id   UUID REFERENCES "answers"(id) ON UPDATE CASCADE,
     CONSTRAINT ck_belongs_to_question_xor_answer CHECK ( (question_id IS NULL) != (answer_id IS NULL) ),
 
     content     VARCHAR(1000) NOT NULL CHECK ( length(content) >= 2 ),
@@ -144,16 +145,16 @@ CREATE TABLE "comments"
     created_at  TIMESTAMP     NOT NULL DEFAULT now(),
     updated_at  TIMESTAMP     NOT NULL DEFAULT now(),
     CONSTRAINT ck_updated_after_created CHECK ( updated_at >= created_at )
-) INHERITS (upvotable);
+) INHERITS ("upvotable");
 
-CREATE TABLE "Image"
+CREATE TABLE Image
 (
     imagePath TEXT NOT NULL
 );
 
 CREATE TABLE "badges"
 (
-    id      SERIAL PRIMARY KEY,
+    id      UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     type    badge_type   NOT NULL,
     title   VARCHAR(25)  NOT NULL CHECK ( length(title) >= 2 ),
     content VARCHAR(100) NOT NULL
@@ -162,8 +163,8 @@ CREATE TABLE "badges"
 CREATE TABLE "user_badges"
 (
     PRIMARY KEY (user_id, badge_id),
-    user_id    SERIAL REFERENCES "users"(id) ON UPDATE CASCADE,
-    badge_id   SERIAL REFERENCES "badges"(id) ON UPDATE CASCADE,
+    user_id    UUID REFERENCES "users"(id) ON UPDATE CASCADE,
+    badge_id   UUID REFERENCES "badges"(id) ON UPDATE CASCADE,
 
     awarded_at TIMESTAMP NOT NULL DEFAULT now()
 );
