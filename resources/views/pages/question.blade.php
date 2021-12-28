0caa525c6dfa25ad02c5745bec82f4fa6df34161
Vasco <vsngarcia5@gmail.com>
2021-12-28 15:02:00 +0000

    Fix hanging div when question has no tags

--- resources/views/pages/question.blade.php
+++ resources/views/pages/question.blade.php
@@ -30,56 +30,55 @@
                                     </div>
                                 </a>
                             </div>
-
                         @endforeach
-                        @endif
+                    </div>
+                @endif
+
+
+                <div class="d-flex">
+                    <div class="text-center pt-2">
+                        @php
+                            $liked = auth()->check() ? $question->likedBy(auth()->user()) : false;
+                            $disliked = auth()->check() ? $question->dislikedBy(auth()->user()) : false;
+                        @endphp
+                        <form action="{{ route('question.review', [$question->id, 'like']) }}" method="post">
+                            @csrf
+                            @if ($liked)
+                                @method('DELETE')
+                            @endif
+                            <button type="submit" class="btn"><i
+                                    class="fs-4 bi bi-chevron-up @if ($liked)) text-primary @endif"></i></button>
+                        </form>
+                        <div>{{ $question->score() }}</div>
+
+                        <form action="{{ route('question.review', [$question->id, 'dislike']) }}" method="post">
+                            @csrf
+                            @if ($disliked)
+                                @method('DELETE')
+                            @endif
+                            <button type="submit" class="btn">
+                                <i class="fs-4 bi bi-chevron-down @if ($disliked) text-danger @endif"></i>
+                            </button>
+                        </form>
                     </div>
 
+                    <div class="flex-grow-1 p-4">
+                        <p>{{ $question->content }}</p>
 
-                    <div class="d-flex">
-                        <div class="text-center pt-2">
-                            @php
-                                $liked = auth()->check() ? $question->likedBy(auth()->user()) : false;
-                                $disliked = auth()->check() ? $question->dislikedBy(auth()->user()) : false;
-                            @endphp
-                            <form action="{{ route('question.review', [$question->id, 'like']) }}" method="post">
-                                @csrf
-                                @if ($liked)
-                                    @method('DELETE')
-                                @endif
-                                <button type="submit" class="btn"><i
-                                        class="fs-4 bi bi-chevron-up @if ($liked)) text-primary @endif"></i></button>
-                            </form>
-                            <div>{{ $question->score() }}</div>
-
-                            <form action="{{ route('question.review', [$question->id, 'dislike']) }}" method="post">
-                                @csrf
-                                @if ($disliked)
-                                    @method('DELETE')
-                                @endif
-                                <button type="submit" class="btn">
-                                    <i class="fs-4 bi bi-chevron-down @if ($disliked) text-danger @endif"></i>
-                                </button>
-                            </form>
-                        </div>
-
-                        <div class="flex-grow-1 p-4">
-                            <p>{{ $question->content }}</p>
-
-                            <div class="d-flex justify-content-end pt-2">
-                                <div style="min-width: 18rem">
-                                    <div>
-                                        Asked {{ \Carbon\Carbon::parse($question->created_at)->toDayDateTimeString() }}
-                                    </div>
-                                    <a class="text-decoration-none"
-                                       href="{{ '/users/' . $question->user->username }}">{{ $question->user->username }}
-                                    </a>
+                        <div class="d-flex justify-content-end pt-2">
+                            <div style="min-width: 18rem">
+                                <div>
+                                    Asked {{ \Carbon\Carbon::parse($question->created_at)->toDayDateTimeString() }}
                                 </div>
+                                <a class="text-decoration-none"
+                                   href="{{ '/users/' . $question->user->username }}">{{ $question->user->username }}
+                                </a>
                             </div>
-
-                            @each('partials.comment',$question->comments, 'comment')
                         </div>
+
+                        @each('partials.comment',$question->comments, 'comment')
                     </div>
+                </div>
             </div>
             <hr>
 
