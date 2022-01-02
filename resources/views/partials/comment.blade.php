8d2753c52e8e02a655a79c5cdc929d28b5d10ce8
Vasco <vsngarcia5@gmail.com>
2022-01-02 16:53:44 +0000

    Edit comment tweaks

--- resources/views/partials/comment.blade.php
+++ resources/views/partials/comment.blade.php
@@ -1,13 +1,44 @@
 <hr>
-<div class="d-flex" >
+<div class="d-flex">
     <div class="px-2 text-muted">
         {{ $comment->score() }}
     </div>
+
     <div>
-        <div id="comment{{$comment->id}}" style="display: block">
+        <div class="comment-{{$comment->id}}">
             {{ $comment->content }}
         </div>
-        <div class="d-flex" id="coisas{{$comment->id}}">
+
+        <form
+            action="{{ route('comment.update', $comment) }}"
+            method="post"
+            class="d flex comment-edit-{{ $comment->id }}"
+            style="display: none;"
+        >
+            @csrf
+            @method('PATCH')
+            <div>
+                <input type="text" name="identifier" style="display: none;"
+                       value="{{ $comment->id }}">
+                <input type="text" name="type" style="display: none;" value="comment">
+                <input class="form-control" name="content" style="margin-right:1.5em; max-width: 80%"
+                       type="text"
+                       value="{{$comment->content}}"
+                       aria-label="default input example">
+
+
+                <button
+                    type="button"
+                    class="btn text-muted p-0"
+                    onclick="swapElements('.comment-{{$comment->id}}', '.comment-edit-{{$comment->id}}', false)"
+                >
+                    Cancel
+                </button>
+                <button class="btn text-primary p-0 px-1" type="submit">Submit</button>
+            </div>
+        </form>
+
+        <div class="d-flex comment-{{$comment->id}}">
             @php
                 $liked = auth()->check() ? $comment->likedBy(auth()->user()) : false;
             @endphp
@@ -25,17 +56,15 @@
             @if(Auth::check() && $comment->user->username == auth()->user()->username)
 
                 {{-- Edit Comment --}}
-                <div class="px-1">
-                    {{-- <form action="" method="post">  {{ route('comment.edit', [$comment->id]) }} --}}
-                        {{-- @csrf --}}
-                        {{-- @method('PATCH') --}}
-
-                        {{-- </form> --}}
-                        <button type="submit" class="btn p-0 text-muted" onclick="changeDivs2({{$comment->id}})"> Edit</button>
+                <div class="px-2">
+                    <button type="submit" class="btn p-0 text-muted"
+                            onclick="swapElements('.comment-{{$comment->id}}', '.comment-edit-{{$comment->id}}', true)">
+                        Edit
+                    </button>
                 </div>
 
                 {{-- Delete Comment --}}
-                <div class="px-1">
+                <div>
                     <form action="{{ route('comment.delete', [$comment]) }}" method="post">
                         @csrf
                         @method('DELETE')
@@ -43,19 +72,7 @@
                         <button type="submit" class="btn p-0 text-muted"> Delete</button>
                     </form>
                 </div>
-
             @endif
-            {{--        <div>{{ $comment->score() }}</div>--}}
-
-            {{--        <form action="{{ route('comment.review', [$comment->id, 'dislike']) }}" method="post">--}}
-            {{--            @csrf--}}
-            {{--            @if ($disliked)--}}
-            {{--                @method('DELETE')--}}
-            {{--            @endif--}}
-            {{--            <button type="submit" class="btn">--}}
-            {{--                <i class="fs-4 bi bi-chevron-down @if ($disliked) text-danger @endif"></i>--}}
-            {{--            </button>--}}
-            {{--        </form>--}}
         </div>
     </div>
 
@@ -63,43 +80,8 @@
         <a class="text-decoration-none"
            href="{{ '/user/'.$comment->user->username }}">{{ $comment->user->username }}</a>
     </div>
+
     <div class="ms-2 text-muted">
-        {{ $comment->created_at }}
+        {{ $comment->created_at . ($comment->created_at != $comment->updated_at ? " (edited)" : "") }}
     </div>
 </div>
-
-{{-- <div class="d flex" id= "comment-edit{{$comment->id}}" style="display: none; width:40rem"> --}}
-    <form action="{{ route('comment.update', $comment) }}" method="post">
-        @csrf
-        @method('PATCH')
-        <div class="d flex" id= "comment-edit{{ $comment->id }}" style="display: none; width:40rem">
-            <input type="text" name="identifier" style="display: none;"
-                   value="{{ $comment->id }}">
-            <input type="text" name="type" style="display: none;" value="comment">
-            <input class="form-control" name="content" style="margin-right:1.5em; max-width: 80%"
-                   type="text"
-                   value={{$comment->content}} aria-label="default input example">
-
-
-                <button type="button" id="edit" class="btn btn-primary" onclick="changeDivs2({{$comment->id}})">
-                    Cancel
-                </button>
-            <input class="btn btn-primary" type="submit" value="Submit">
-
-        </div>
-    </form>
-{{-- </div> --}}
-
-<script>
-    function changeDivs2(id) {
-        var zz = document.getElementById("comment"+id);
-        var z = document.getElementById("comment-edit"+id);
-        if (z.style.display === "none") {
-            z.style.display = "block";
-            zz.style.display = "none";
-        } else {
-            z.style.display = "none";
-            zz.style.display = "block";
-        }
-    }
-</script>
